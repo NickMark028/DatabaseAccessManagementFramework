0b93a3d57057754925a7c031f863356020b4bbab
Nick Mark <nickmark028@gmail.com>
2021-12-13 13:44:17 +0700

    Setup solution

---
+++ DatabaseAccessManagement/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DatabaseAccessManagement
+{
+	public class PublicClass
+	{
+		public void Print()
+		{
+			Console.WriteLine("Hi");
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DatabaseAccessManagement;
+
+namespace Demo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			PublicClass pc = new PublicClass();
+			pc.Print();
+
+			Console.ReadKey();
+		}
+	}
+}
