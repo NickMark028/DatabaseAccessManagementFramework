09ba21e219d9b23f789e4a4ef40bf5de4a490202
Nick Mark <nickmark028@gmail.com>
2021-12-21 22:29:31 +0700

    Add CRUD method for database

--- DatabaseAccessManagement/DatabaseFactory/DatabaseFactory.cs
+++ DatabaseAccessManagement/DatabaseFactory/DatabaseFactory.cs
@@ -3,38 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tutorial.SqlConn;
 using MySql.Data.MySqlClient;
 
-namespace DatabaseAccessManagement.DatabaseFactory
+namespace DatabaseAccessManagement
 {
-	abstract class Database
+	public interface IDatabase
 	{
-		public abstract Iconnection createConnection();
-
+		IConnection CreateConnection();
 	}
 
-	public class MySql : Database
-    {
-		private string host ;
-        private int port ;
-        private string database ;
-        private string username ;
-        private string password ;
-		public MySql(string host, int port, string database,string username,string password) { 
+	public class MySql : IDatabase
+	{
+		private string host;
+		private int port;
+		private string username;
+		private string password;
+		private string database;
+
+		public MySql(string host, int port, string database, string username, string password)
+		{
 			this.host = host;
 			this.port = port;
 			this.database = database;
 			this.username = username;
-			this.password=password
+			this.password = password;
 		}
-		public override Iconnection createConnection()
-        {
-			  String connString = "Server=" + host + ";Database=" + database
-                + ";port=" + port + ";User Id=" + username + ";password=" + password;
+		public IConnection CreateConnection()
+		{
+			string connString = "Server=" + host + ";Database=" + database
+				+ ";port=" + port + ";User Id=" + username + ";password=" + password;
 
-            MySqlConnection conn = new MySqlConnection(connString);
-			return new MysqlConnectionAdapter(conn)
-        }
-    }
+			MySqlConnection conn = new MySqlConnection(connString);
+			return new MySqlConnectionAdapter(conn);
+		}
+	}
 }
--- DatabaseAccessManagement/DatabaseFactory/Iconnection.cs
+++ DatabaseAccessManagement/DatabaseFactory/Iconnection.cs
@@ -3,35 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tutorial.SqlConn;
 using MySql.Data.MySqlClient;
 
-interface Iconnection
+namespace DatabaseAccessManagement
 {
-    void close();
-    void runRawQuery(string query);
-}
+	public interface IConnection
+	{
+		void RunRawQuery(string query);
+		void Insert<T>(object[] rows);
+		void Delete<T>(SQLPredicate predicate);
+		void Update<T>(SQLPredicate predicate, object newValue);
+		void Close();
+	}
 
-class MysqlConnectionAdapter : Iconnection
-{
-    MysqlConnectionAdapter(MySqlConnection connection)
-    {
-        this.connection = connection;
-    }
-    
-    
-    MySqlConnection connection 
-    public void close()
-    {
-        connection.close();
-        connection.Dispose();
-    }
-    public void runRawQuery(string query)
-    {
-         MySqlCommand cmd = new MySqlCommand(query, connection);
-      //if select
-        MySqlDataReader rdr = cmd.ExecuteReader();       
-      //if insert/update/delete
-       cmd.ExecuteNonQuery()
-    }
+	public class MySqlConnectionAdapter : IConnection
+	{
+		private MySqlConnection connection;
+
+		public MySqlConnectionAdapter(MySqlConnection connection)
+		{
+			this.connection = connection;
+		}
+
+		public void RunRawQuery(string query)
+		{
+			MySqlCommand cmd = new MySqlCommand(query, connection);
+			//if select
+			MySqlDataReader rdr = cmd.ExecuteReader();
+			//if insert/update/delete
+			cmd.ExecuteNonQuery();
+
+		}
+		public void Insert<T>(object[] rows)
+		{
+			throw new NotImplementedException();
+		}
+		public void Delete<T>(SQLPredicate predicate)
+		{
+			throw new NotImplementedException();
+		}
+		public void Update<T>(SQLPredicate predicate, object newValue)
+		{
+			throw new NotImplementedException();
+		}
+		public void Close()
+		{
+			connection.Close();
+			connection.Dispose();
+		}
+	}
 }
