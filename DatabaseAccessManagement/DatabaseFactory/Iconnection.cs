02923964ee5a4c93a6b3e11f6f05ed8cd196c36d
thanhvu2901 <vcthanh2901@gmail.com>
2021-12-21 17:55:41 +0700

    connection

---
+++ DatabaseAccessManagement/DatabaseFactory/Iconnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorial.SqlConn;
+using MySql.Data.MySqlClient;
+
+interface Iconnection
+{
+    void close();
+    void runRawQuery(string query);
+}
+
+class MysqlConnectionAdapter : Iconnection
+{
+    MysqlConnectionAdapter(MySqlConnection connection)
+    {
+        this.connection = connection;
+    }
+    MySqlConnection connection 
+    public void close()
+    {
+        connection.close();
+    }
+    public void runRawQuery(string query)
+    {
+        connection.ExecuteNonQuery(query);
+    }
+}
