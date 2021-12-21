d113200c34e5d766274394b829e460d197638655
Nick Mark <nickmark028@gmail.com>
2021-12-21 14:54:50 +0700

    Add QueryBuilder

---
+++ DatabaseAccessManagement/Query/QueryBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseAccessManagement.Query
+{
+	interface ISQLPredicate { }
+
+	abstract class QueryBuilder
+	{
+		public abstract void Select();
+		public abstract void Select(params string[] columns);
+		public abstract void Where(ISQLPredicate predicate);
+		public object[] Execute()
+		{
+			string rawSQL = ToRawQueryString();
+			return null;	// Todo: asdas
+		}
+
+		protected abstract string ToRawQueryString();
+	}
+}
