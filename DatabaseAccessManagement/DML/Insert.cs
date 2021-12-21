a85f7941dda51d5323a422448a2af3502ca52362
Nick Mark <nickmark028@gmail.com>
2021-12-21 21:31:53 +0700

    Create Insert.cs

---
+++ DatabaseAccessManagement/DML/Insert.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseAccessManagement
+{
+	public class StudentA
+	{
+		public int Id;
+		public string Name;
+		public float Score;
+	}
+
+
+
+	public class DML
+	{
+		public void Insert<A>()
+		{
+
+		}
+	}
+
+	public class MainProgram
+	{
+		public static void MainTest()
+		{
+			var x =new DML();
+			x.Insert<StudentA>(new StudentA { Id = 10, Name ="Nguye Van A", Score = 1.4f});
+		}
+	}
+}
