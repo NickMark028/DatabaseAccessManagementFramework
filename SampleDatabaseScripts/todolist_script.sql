8a450e3bb5358259c7174851fff988f49df6297f
thanhvu2901 <vcthanh2901@gmail.com>
2022-01-06 23:14:45 +0700

    to do list script

---
+++ SampleDatabaseScripts/todolist_script.sql
@@ -0,0 +1,20 @@
+use todolist;
+
+create table task (
+id int primary key auto_increment,
+task varchar(1000),
+isdone bool
+);
+
+insert into task (task,isdone) values 
+('test',false),
+('test1',true),
+('test2',false),
+('test3',true),
+('test4',false),
+('test5',false),
+('test6',true),
+('test7',false),
+('test8',true),
+('test9',true)
+;
