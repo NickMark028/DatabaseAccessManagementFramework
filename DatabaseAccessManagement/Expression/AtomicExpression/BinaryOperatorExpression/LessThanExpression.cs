30b116c8a909aac6b3fde3b128dee3eac32759db
MrSooo <51949526+MrSooo@users.noreply.github.com>
2022-01-07 16:13:00 +0700

    +Expand constructors of LessThanExpression

--- DatabaseAccessManagement/Expression/AtomicExpression/BinaryOperatorExpression/LessThanExpression.cs
+++ DatabaseAccessManagement/Expression/AtomicExpression/BinaryOperatorExpression/LessThanExpression.cs
@@ -1,8 +1,19 @@
-﻿namespace DatabaseAccessManagement
+﻿using System;
+namespace DatabaseAccessManagement
 {
 	public class LessThanExpression : BinaryOperatorPredicate
 	{
 		public LessThanExpression(string leftMember, string rightMember)
+			: base(leftMember, rightMember) { }
+		public LessThanExpression(string leftMember, Int64 rightMember)
+			: base(leftMember, rightMember) { }
+		public LessThanExpression(string leftMember, Decimal rightMember)
+			: base(leftMember, rightMember) { }
+		public LessThanExpression(string leftMember, Double rightMember)
+			: base(leftMember, rightMember) { }
+		public LessThanExpression(string leftMember, DateTime rightMember)
+		: base(leftMember, rightMember) { }
+		public LessThanExpression(string leftMember, Boolean rightMember)
 			: base(leftMember, rightMember) { }
 
 		public override string ToString()
