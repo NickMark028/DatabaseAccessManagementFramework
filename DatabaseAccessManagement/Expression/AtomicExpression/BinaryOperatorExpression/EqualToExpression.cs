3be74810da51340d10bd7ee0cd993c3702a08e2c
MrSooo <51949526+MrSooo@users.noreply.github.com>
2022-01-07 16:10:01 +0700

    +Expand constructors of EqualToExpression

--- DatabaseAccessManagement/Expression/AtomicExpression/BinaryOperatorExpression/EqualToExpression.cs
+++ DatabaseAccessManagement/Expression/AtomicExpression/BinaryOperatorExpression/EqualToExpression.cs
@@ -1,8 +1,20 @@
-﻿namespace DatabaseAccessManagement
+﻿using System;
+namespace DatabaseAccessManagement
 {
 	public class EqualToExpression : BinaryOperatorPredicate
 	{
 		public EqualToExpression(string leftMember, string rightMember)
+			: base(leftMember, rightMember) { }
+
+		public EqualToExpression(string leftMember, Int64 rightMember)
+			: base(leftMember, rightMember) { }
+		public EqualToExpression(string leftMember, Decimal rightMember) 
+			: base(leftMember, rightMember) { }
+		public EqualToExpression(string leftMember, Double rightMember)
+			: base(leftMember, rightMember) { }
+		public EqualToExpression(string leftMember, DateTime rightMember)
+		: base(leftMember, rightMember) { }
+		public EqualToExpression(string leftMember, Boolean rightMember)
 			: base(leftMember, rightMember) { }
 
 		public override string ToString()
